7e3b80e2995c36b04ae4db61d347825479c1e294
Jan Schrewe <jschrewe@gmail.com>
2023-05-08 18:06:30 +0200

    Update urls.py

--- urls.py
+++ urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from .views import authenticate_view, get_user_view
+from .views import authenticate_view, get_user_view, multiplikatoren_api
 
 app_name='authprovider'
 
 urlpatterns = [
     path("authenticate/", authenticate_view, name="authenticate"),
     path("getuser/<int:user_id>/", get_user_view, name="get_user"),
+    path("getmultis/<int:user_id>/", multiplikatoren_api, name=multiplikatoren_api),
 ]
